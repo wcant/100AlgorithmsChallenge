629bf837716f9e67a088f78935b2b8853a2f8754
Wesley Cantrell <mail@init1.io>
2021-02-17 18:31:48 -0600

    updating addBorder

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,17 @@
 export function addBorder(picture: string[]): string[] {
 
+    const borderLen = picture[0].length;
+    const border = '';
+
+    for (let i = 1; i <= borderLen; i++) {
+        "*".concat(border);
+        console.log(i, border);
+    }
+
+    let arrWithBorder = [...picture];
+
+
+    console.log(border);
 }
 
 // console.log(addBorder(["abc", "ded"]));
