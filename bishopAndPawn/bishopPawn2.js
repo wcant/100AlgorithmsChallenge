d55e35890e2f79f6553a19fbf70e9d31148776a5
Wesley Cantrell <mail@init1.io>
2021-11-22 19:28:54 -0600

    update

--- bishopAndPawn/bishopPawn2.js
+++ bishopAndPawn/bishopPawn2.js
@@ -1,5 +1,23 @@
 export function bishopAndPawn(bishop: string, pawn: string): boolean {
 
+    const xCoords = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    const yCoords = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
+    const bishopX = bishop[0];
+    const bishopXInd = xCoords.indexOf(bishopX);
+    const bishopY = parseInt(bishop[1]);
+    const bishopYInd = bishopY-1;
+
+    
+    if (bishopYInd > bishopXInd) {
+        const shiftLeft = 
+        const shiftRight
+        
+    } else if (bishopYInd === bishopXInd) {
+
+    } else {
+        // const shiftLeft
+        // const shiftRight
+    }
 
 }
